ced8fad36119130aa98efa17b5b17fa45c16a728
Joao Felipe Lemos <lemos391@gmail.com>
2023-04-20 22:35:52 -0300

    Initial commit
    Generated by create-expo-app 1.3.2.

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
